4ed346fac1b95823b5be1a03334b4768acd9407e
FahreddinSevindir <fahreddinsevindir@gmail.com>
2020-12-21 16:11:27 +0300

    PagingSource & PagingData

---
+++ app/src/main/java/com/fahreddinsevindir/animalphotoapp/data/UnsplashPagingSource.kt
@@ -0,0 +1,33 @@
+package com.fahreddinsevindir.animalphotoapp.data
+
+import androidx.paging.PagingSource
+import com.fahreddinsevindir.animalphotoapp.api.UnsplashApi
+import java.lang.Exception
+
+private const val UNSPLASH_STARTING_PAGE_INDEX = 1
+
+class UnsplashPagingSource(
+    private val unsplashApi: UnsplashApi,
+    private val query: String
+) : PagingSource<Int, UnsplashPhoto>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, UnsplashPhoto> {
+        val position = params.key ?: UNSPLASH_STARTING_PAGE_INDEX
+
+        return try {
+            val response = unsplashApi.searchPhotos(query, position, params.loadSize)
+            val photos = response.results
+
+            LoadResult.Page(
+                data = photos,
+                prevKey = if (position == UNSPLASH_STARTING_PAGE_INDEX) null else position - 1,
+                nextKey = if (photos.isEmpty()) null else position + 1
+            )
+        } catch (exception: Exception) {
+            LoadResult.Error(exception)
+        } catch (exception: Exception) {
+            LoadResult.Error(exception)
+        }
+
+    }
+}
--- app/src/main/java/com/fahreddinsevindir/animalphotoapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/fahreddinsevindir/animalphotoapp/ui/gallery/GalleryFragment.kt
@@ -1,9 +1,23 @@
 package com.fahreddinsevindir.animalphotoapp.ui.gallery
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
 import com.fahreddinsevindir.animalphotoapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+
+    private val viewModel by viewModels<GalleryViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+            
+        }
+    }
 }
